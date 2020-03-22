db38c532b33a3a73cec913cfba5f9f01a0bd068c
Kaylee210 <kaylee.nov.210@gmail.com>
2020-03-22 17:59:08 +0900

    config 修正

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,5 @@
 module.exports = {
-    DB_URL: 'mongodb+srv://test:testtest@cluster0-tr9sv.mongodb.net/test?retryWrites=true&w=majority'
+    // 誰にも見せたくない情報はここにかく
+    DB_URL: 'mongodb+srv://test:testtest@cluster0-tr9sv.mongodb.net/test?retryWrites=true&w=majority',
+    SECRET: 'y/B?E(H+MbQeThWmZq4t9a!z%c&F)J@N'
 }
